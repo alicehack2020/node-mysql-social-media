7faa48fb7cf191ab7675e95a274ef2efa4f9e1d4
alicehack2020 <alicehack2020@gmail.com>
2024-03-23 22:44:04 +0530

    logout added

--- controllers/auth.js
+++ controllers/auth.js
@@ -57,14 +57,22 @@ export const login = (req, res) => {
     }
 
     const token = jwt.sign({ id: result[0].id }, "secreatekey");
-    const { password, ...data } = result[0];
+    const { password, ...others } = result[0];
 
     res
       .cookie("accessToken", token, {
         httpOnly: true,
       })
       .status(200)
-      .json(data);
+      .json(others);
   });
 };
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User has been logged out.");
+};
