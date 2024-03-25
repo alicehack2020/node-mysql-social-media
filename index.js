89662442fc01a9412348b70693ef242957f8ddff
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2024-03-25 13:41:48 +0530

    Corssetup (#2)
    * post started
    
    * moment used, post crud
    
    * multer added for image upload

--- index.js
+++ index.js
@@ -7,16 +7,41 @@ import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import multer from "multer";
 //env
 dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
 //middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../frontend/public");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
